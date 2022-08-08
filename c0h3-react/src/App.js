47ea021b4528739c5cb1b2ec7334e5c39ee4b69d
Vladislav Korecký <vladislav.ml@korecky.org>
2022-08-08 14:21:16 +0200

    Hello world front-end

--- c0h3-react/src/App.js
+++ c0h3-react/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+    const [heading, setHeading] = useState("")
+
+    useEffect(() => {
+        fetch("http://127.0.0.1:5000/")
+            .then(res => {
+                if (res.ok) {
+                    return res.json()
+                }
+            })
+            .then(data => {
+                setHeading(data.message)
+            })
+            .then(error => console.log(error))
+    }, [])
+
+
+    return (
+        <h1>{heading}</h1>
+    )
 }
-
-export default App;
