19c3c852ab1c652ba7dcb4265e3cc3bb2ac47e47
Vladislav Korecký <vladislav.ml@korecky.org>
2022-08-09 19:43:02 +0200

    Adding documentation

--- c0h3-react/src/App.js
+++ c0h3-react/src/App.js
@@ -1,9 +1,28 @@
+// @ts-check
+
 import { useState } from "react";
 import Form from "./components/Form";
 
-
+/**
+ * Root app component.
+ * 
+ * @type {function}
+ * @returns {Object} - App component's JSX.
+ * 
+ * @component
+ */
 export default function App() {
+    /**
+     * Testing this out.
+     */
     const [searchResults, setSearchResults] = useState([])
+
+    /**
+     * Testing this our. But there is a twist!
+     * 
+     * @type {string}
+     */
+    const randomVar = "Lol"
 
     return (
         <>
