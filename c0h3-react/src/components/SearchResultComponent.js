530a5c5e3369ba586acad5db586a24d9f7f9c38d
Vladislav Korecký <vladislav.ml@korecky.org>
2022-08-10 15:02:10 +0200

    Adding a SearchResultComponent

---
+++ c0h3-react/src/components/SearchResultComponent.js
@@ -0,0 +1,25 @@
+// @ts-check
+
+import React from "react";
+
+
+/**
+ * A single search result component.
+ * @see {@link Form} for more information about the search mechanics.
+ * @see {@link SearchResultsDisplay} - A parent component.
+ * 
+ * Possible props:
+ *      text - A text/string to display.
+ * 
+ * @param {Object} props - The React properties (props).
+ * @returns {Object} - Form component's JSX.
+ * 
+ * @component
+ */
+export default function SearchResultComponent({text}) {
+    return (
+        <div>
+
+        </div>
+    )
+}
